f32614b18f539072d42ca8b4be2e60534639cc51
Mika88 <mikaspivak@gmail.com>
2019-03-16 21:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  let distance
+  if (block > 42) {
+    distance = block - 42;
+  } else {
+    distance = 42 - block;
+  }
+  return distance
+}
